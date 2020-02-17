685afeece2d65bf1b1c2fbe4ca9c79e6901af22e
Ogrebatel <kotkov.oleg.2011@yandex.ru>
2020-02-17 18:03:30 +0300

    0.3

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,9 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-
+#include <header.hpp>
 TEST(Example, EmptyTest) {
+    my_hash test;
+    test.start();
     EXPECT_TRUE(true);
 }
